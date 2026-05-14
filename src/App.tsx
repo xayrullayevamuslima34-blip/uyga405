93217468f3f90474cdbfa29014cc3e94fec9a1ec
Muslima Xayrullayeva <muslimaxayrullayeva@Muslimas-MacBook-Pro.local>
2026-05-14 16:20:13 +0500

    joylashuvi to'g'irlandi

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,61 @@
 export default function App() {
-  return (
-      <div className="bg-[#202020] min-h-screen text-white p-6 font-sans">
-        <h1 className="text-3xl font-bold mb-2">Top kitoblar</h1>
-        <p className="text-gray-400 text-sm mb-6">Barchasi</p>
+    return (
+        <div className="bg-[#202020] min-h-screen text-white p-6 font-sans">
+            <div className="flex items-start gap-70">
+            <h1 className="text-3xl font-bold mb-2">Top kitoblar</h1>
+            <p className="text-gray-400 text-sm mb-6">Barchasi &gt; </p>
+            </div>
 
-        <div className="space-y-6">
-          <img
-              src="/image1.jpg"
-              alt="Shaxmat kitobi"
-              className="w-28 h-28.25 object-cover rounded-sm"
-          />
-          <div>
-            <h3 className="font-semibold">Shaxmatdagi qobiliyatlaringizga qayta baho bering</h3>
-            <p className="text-gray-400 text-sm">J.Silman</p>
-          </div>
+            <div className="space-y-6">
+                <div className="flex items-start gap-1.5">
+                    <img
+                        src="/image1.jpg"
+                        alt="Shaxmat kitobi"
+                        className="w-28 h-28.25 object-cover rounded-sm"
+                    />
+                    <div>
+                        <h3 className="font-semibold">Shaxmatdagi qobiliyatlaringizga qayta baho bering</h3>
+                        <p className="text-gray-400 text-sm">J.Silman</p>
+                    </div>
+                </div>
 
-
-          <img
-              src="/image2.jpg"
-              alt="Mening tizimim"
-              className="w-28 h-28.25 object-cover rounded-sm"
-          />
-          <div>
-            <h3 className="font-semibold">Mening tizimim</h3>
-            <p className="text-gray-400 text-sm">A.Nimzowitsch</p>
-          </div>
-
-
-          <img
-              src="/image3.jpg"
-              alt="Shaxmat musobaqasi"
-              className="w-28 h-28.25 object-cover rounded-sm"
-          />
-          <div>
-            <h3 className="font-semibold">Zurixdagi shaxmat musobaqasi</h3>
-            <p className="text-gray-400 text-sm">D.Bronstein</p>
-          </div>
+                <div className="flex items-start gap-1.5">
+                <img
+                    src="/image2.jpg"
+                    alt="Mening tizimim"
+                    className="w-28 h-28.25 object-cover rounded-sm"
+                />
+                <div>
+                    <h3 className="font-semibold">Mening tizimim</h3>
+                    <p className="text-gray-400 text-sm">A.Nimzowitsch</p>
+                </div>
+                </div>
 
 
-          <img
-              src="/image4.jpg"
-              alt="O'yinlar"
-              className="w-28 h-28.25 object-cover rounded-sm"
-          />
-          <div>
-            <h3 className="font-semibold">BOBBY FISCHER — my 60 memorable games</h3>
-            <p className="text-gray-400 text-sm">Mening esda qolarli o'yinlarim • B.Fischer</p>
-          </div>
+                <div className="flex items-start gap-1.5">
+                <img
+                    src="/image3.jpg"
+                    alt="Shaxmat musobaqasi"
+                    className="w-28 h-28.25 object-cover rounded-sm"
+                />
+                <div>
+                    <h3 className="font-semibold">Zurixdagi shaxmat musobaqasi</h3>
+                    <p className="text-gray-400 text-sm">D.Bronstein</p>
+                </div>
+                </div>
+
+                <div className="flex items-start gap-1.5">
+                <img
+                    src="/image4.jpg"
+                    alt="O'yinlar"
+                    className="w-28 h-28.25 object-cover rounded-sm"
+                />
+                <div>
+                    <h3 className="font-semibold">BOBBY FISCHER — my 60 memorable games</h3>
+                    <p className="text-gray-400 text-sm">Mening esda qolarli o'yinlarim • B.Fischer</p>
+                </div>
+                </div>
+            </div>
         </div>
-      </div>
-  )
+    )
 }
